2d5af70f000fc8f0532b200ecd22c34d4657588d
Gyeongmin Park <ddogi1513@naver.com>
2025-01-07 00:45:25 +0900

    solve part2

--- src/aoc2024/day5.clj
+++ src/aoc2024/day5.clj
@@ -31,5 +31,33 @@
          (map middle-element)
          (apply +))))
 
+(defn find-valid-order
+  [graph path visited order]
+  (let [node (last path)
+        candidates (->> (get graph node)
+                        (remove visited))]
+    (if (= (count path) (count order))
+      path
+      (some #(find-valid-order graph (conj path %) (conj visited %) order) candidates))))
+
+(defn reorder
+  [ordering-rules order]
+  (let [order-set (set order)
+        ordering-graph (->> ordering-rules
+                            (filter (fn [rule] (every? order-set rule)))
+                            (reduce (fn [graph [from to]]
+                                      (update graph from conj to))
+                                    {}))]
+    (some #(find-valid-order ordering-graph [%] #{%} order) order)))
+
+(defn solve-part2
+  [file-name]
+  (let [{:keys [ordering-rules orders]} (parse-input file-name)]
+    (->> orders
+         (remove #(valid-order? ordering-rules %))
+         (map #(reorder ordering-rules %))
+         (map middle-element)
+         (apply +))))
+
 (comment
-  (solve-part1 "day5.txt"))
+  (solve-part2 "day5.txt"))
