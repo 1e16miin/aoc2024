094a8d860c62ac97e030766e64da3f049a317d7a
Gyeongmin Park <ddogi1513@naver.com>
2025-01-09 22:56:40 +0900

    solve part2

--- src/aoc2024/day7.clj
+++ src/aoc2024/day7.clj
@@ -12,36 +12,45 @@
        (map parse-line)))
 
 (defn generate-operations
-  [n]
+  [n ops]
   (if (= n 1)
     [[]]
-    (let [ops [\* \+]]
-      (->> (generate-operations (dec n))
-           (mapcat (fn [seq]
-                     (map #(conj seq %) ops)))))))
+    (->> (generate-operations (dec n) ops)
+         (mapcat (fn [seq]
+                   (map #(conj seq %) ops))))))
 
 (defn calculate
-  [[start & rests] operations]
+  [result [start & rests] operations]
   (reduce
-   (fn [result [op number]]
-     (if (= op \*)
-       (* result number)
-       (+ result number)))
+   (fn [partial [op number]]
+     (if (> partial result)
+       (reduced partial)
+       (case op
+         "*" (* partial number)
+         "+" (+ partial number)
+         (parse-long (str partial number)))))
    start
    (map vector operations rests)))
 
 (defn equal?
-  [[result & numbers]]
-  (let [operations-sets (generate-operations (count numbers))
-        calculate-results (map #(calculate numbers %) operations-sets)]
+  [ops [result & numbers]]
+  (let [operations-sets (generate-operations (count numbers) ops)
+        calculate-results (map #(calculate result numbers %) operations-sets)]
     (contains? (set calculate-results) result)))
 
 (defn solve-part1
   [file-name]
   (->> (get-equations file-name)
-       (filter equal?)
+       (filter #(equal? ["*" "+"] %))
+       (map first)
+       (apply +)))
+
+(defn solve-part2
+  [file-name]
+  (->> (get-equations file-name)
+       (filter #(equal? ["||" "*" "+"] %))
        (map first)
        (apply +)))
 
 (comment
-  (solve-part1 "day7.txt"))
+  (solve-part2 "day7.txt"))
